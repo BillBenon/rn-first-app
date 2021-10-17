ef4bc4b3baebf474247d3f59741b6d0cc8dc248d
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-10-17 20:24:24 +0200

    getting started with styles

--- App.js
+++ App.js
@@ -1,28 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState(
-    "Open up App.js to start working on your app!"
-  );
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        title="Change Text"
-        onPress={() => setOutputText("The text changed!")}
-      />
+    <View style={{ padding: 50 }}>
+      <View>
+        <TextInput
+          placeholder="Course Goal"
+          style={{ borderColor: "black", borderWidth: 1, padding: 10 }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
